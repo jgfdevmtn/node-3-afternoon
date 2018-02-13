c818f641ff869efdf32771b981dbd5396fb99ea0
Owner <owner@Owners-MacBook-Pro.local>
2018-02-13 14:22:46 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const checkForSession = require './middlewares/checkForSession'
 const swag_controller = require('./controllers/swag_controller')
 const auth_controller = require('./controllers/auth_controller')
 const cart_controller = require('./controllers/cart_controller')
-
+const search_controller = require('./controller/search_controller')
 
 const app = express()
 
@@ -28,6 +28,12 @@ app.post( '/api/login', auth_controller.login)
 app.post( 'api/register', auth_controller.register)
 app.post( '/api/signout', auth_controller.signout)
 app.get( '/api/user', auth_controller.getUser )
+
+app.post( '/api/cart', cart_controller.add );
+app.post( '/api/cart/checkout', cart_controller.checkout );
+app.delete( '/api/cart', cart_controller.delete );
+
+app.get( '/api/search', search_controller.search );
 
 const port = process.env.PORT || 3000
 
